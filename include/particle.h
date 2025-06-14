27409cedd8aa08c590ce52e4ab5ffed5c8214303
ThaielSC <tmsantiago1@miuandes.cl>
2025-06-14 17:48:25 -0400

    feature(particle): implementa clase particle

---
+++ include/particle.h
@@ -0,0 +1,9 @@
+class Particle {
+private:
+  double mass;
+  double initial_position;
+  double initial_velocity;
+
+public:
+  Particle(double, double, double);
+};
---
+++ src/particle.cpp
@@ -0,0 +1,3 @@
+#include "../include/particle.h"
+
+Particle::Particle(double mass, double init_pos, double init_vel) {}
