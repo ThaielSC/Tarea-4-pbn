6eac92689f7334cab638da31e74fde8a0c8c5fd3
ThaielSC <tmsantiago1@miuandes.cl>
2025-06-22 18:15:33 -0400

    feature(test): Example test finished

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,3 +1,4 @@
+#include <utility>
 #define CATCH_CONFIG_MAIN
 #include "../include/simulation.h"
 #include "catch.hpp"
@@ -13,26 +14,43 @@ TEST_CASE("Collision_time Example", "[Example][Time]") {
   auto s = Simulation(0.2, 9.81);
   auto a = Particle(2, 0, 5);
   auto b = Particle(1, 10, -3);
-  REQUIRE(s.collision_time(a, b) == 1.25);
+
+  REQUIRE(s.collision_time(a, b) == Approx(1.25));
 }
 
 TEST_CASE("Collision_position Example", "[Example][Position]") {
   auto s = Simulation(0.2, 9.81);
   auto a = Particle(2, 0, 5);
   auto b = Particle(1, 10, -3);
-  REQUIRE(s.collision_position(a, b) == 4.7171875);
+
+  REQUIRE(s.collision_position(a, b) == Approx(4.7171875));
 }
 
-// TEST_CASE("Velocity_before_collision Example", "[Example][Velocity]") {
-//   auto s = Simulation(0.2, 9.81);
-//   auto a = Particle(2, 0, 5);
-//   auto b = Particle(1, 10, -3);
-//   REQUIRE(s.velocity_after_collision(a, b) == {-0.33, 7.66});
-// }
+TEST_CASE("Velocity_after_collision Example", "[Example][Velocity]") {
+  auto s = Simulation(0.2, 9.81);
+  auto a = Particle(2, 0, 5);
+  auto b = Particle(1, 10, -3);
 
-// TEST_CASE("Final_distance Example", "[Example][Collision]") {
-//   auto s = Simulation(0.2, 9.81);
-//   auto a = Particle(2, 0, 5);
-//   auto b = Particle(1, 10, -3);
-//   REQUIRE(s.final_distance(a, b) == {0.0283,14.953});
-// }
+  auto expected = std::make_pair(-0.3333333333, 7.6666666667);
+
+  auto result = s.velocity_after_collision(a, b);
+
+  REQUIRE(result.first == Approx(expected.first));
+  REQUIRE(result.second == Approx(expected.second));
+}
+
+TEST_CASE("Final_distance Example", "[Example][Distance]") {
+  auto s = Simulation(0.2, 9.81);
+  auto a = Particle(2, 0, 5);
+  auto b = Particle(1, 10, -3);
+
+  auto post_collision_velocities = s.velocity_after_collision(a, b);
+
+  auto a_after_collision =
+      Particle(a.get_mass(), 0, post_collision_velocities.first);
+  auto b_after_collision =
+      Particle(b.get_mass(), 0, post_collision_velocities.second);
+
+  REQUIRE(s.final_distance(a, b).first == Approx(0.0283).margin(0.001));
+  REQUIRE(s.final_distance(a, b).second == Approx(14.953).margin(0.01));
+}
